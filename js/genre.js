1ea957c0f79fb60be957339182b3b362ebd25162
Hiro11190422 <hiro@toshimahiroshi-no-MacBook-Air.local>
2017-11-15 07:14:28 +0900

    update files 201711150714

--- js/genre.js
+++ js/genre.js
@@ -18,10 +18,14 @@ if( navigator.geolocation ){
         });}
             
            for(var x=1; x < 3; x++){
-              
+              if(cl[x][7]!=""){
             //h.innerHTML += "<br/><h4><a href='" + hh[a][7] + "'>" + hh[a][1] + "</a></h4>ジャンル：" + hh[a][14] + "<br/>住所：" + hh[a][2] + "<br/>営業時間：" + hh[a][3] + "<br/>定休日：" + hh[a][4] + "<br/>電話番号：" + hh[a][6] + "<br/>" + hh[a][8] + "<br/><br/>";
-            c.innerHTML += "<br/><h4><a href='" + cl[x][7] + "'>" + cl[x][1] + "</a></h4>ジャンル：" + cl[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + cl[x][2] + "&dirflg=w'>" + cl[x][2] + "</a><br/>営業時間：" + cl[x][8] + "<br/>定休日：" + cl[x][9] + "<br/>電話番号：" + cl[x][3] + "<br/>" + cl[x][6] + "<br/><br/>";
+            c.innerHTML += "<br/><h4><a href='" + cl[x][7] + "'>" + cl[x][1] + "</a></h4>ジャンル：" + cl[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + cl[x][2] + "&dirflg=w'>" + cl[x][2] + "</a><br/>営業時間：" + cl[x][8] + "<br/>定休日：" + cl[x][9] + "<br/>電話番号：<a hlef='tel:" + cl[x][14] + "'>" + cl[x][3] + "</a><br/>" + cl[x][6] + "<br/><br/>";
+              }
+              else{
+                  c.innerHTML += "<br/><h4>" + cl[x][1] + "</h4>ジャンル：" + cl[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + cl[x][2] + "&dirflg=w'>" + cl[x][2] + "</a><br/>営業時間：" + cl[x][8] + "<br/>定休日：" + cl[x][9] + "<br/>電話番号：<a hlef='tel:" + cl[x][14] + "'>" + cl[x][3] + "</a><br/>" + cl[x][6] + "<br/><br/>";
             
+              }
             
            }
         }
@@ -39,8 +43,12 @@ $(function() {
             var e = document.getElementById("eat");
             
            for(var x=3; x < 14; x++){
-                e.innerHTML += "<br/><h4><a href='" + ea[x][7] + "'>" + ea[x][1] + "</a></h4>ジャンル：" + ea[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr="+ea[x][2]+"&dirflg=w'>" + ea[x][2]+ "</a><br/>営業時間：" + ea[x][8] + "<br/>定休日：" + ea[x][9] + "<br/>電話番号：" + ea[x][3] + "<br/>" + ea[x][6] + "<br/><br/>";
-                
+               if(ea[x][7]!=""){
+                e.innerHTML += "<br/><h4><a href='" + ea[x][7] + "'>" + ea[x][1] + "</a></h4>ジャンル：" + ea[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr="+ea[x][2]+"&dirflg=w'>" + ea[x][2]+ "</a><br/>営業時間：" + ea[x][8] + "<br/>定休日：" + ea[x][9] + "<br/>電話番号：<a hlef='tel:" + ea[x][14] + "'>" + ea[x][3] + "</a><br/>" + ea[x][6] + "<br/><br/>";
+               }
+               else{
+                   e.innerHTML += "<br/><h4>" + ea[x][1] + "</h4>ジャンル：" + ea[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr="+ea[x][2]+"&dirflg=w'>" + ea[x][2]+ "</a><br/>営業時間：" + ea[x][8] + "<br/>定休日：" + ea[x][9] + "<br/>電話番号：<a hlef='tel:" + ea[x][14] + "'>" + ea[x][3] + "</a><br/>" + ea[x][6] + "<br/><br/>";
+               }
             }
         }
     });
@@ -58,9 +66,13 @@ $(function() {
             var m = document.getElementById("magazine");
             
            for(var x=14; x < 20; x++){
-              
-            m.innerHTML += "<br/><h4><a href='" + ma[x][7] + "'>" + ma[x][1] + "</a></h4>ジャンル：" + ma[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + ma[x][2] + "&dirflg=w'>" + ma[x][2] + "</a><br/>営業時間：" + ma[x][8] + "<br/>定休日：" + ma[x][9] + "<br/>電話番号：" + ma[x][3] + "<br/>" + ma[x][6] + "<br/><br/>";
+              if(ma[x][7]!=""){
+            m.innerHTML += "<br/><h4><a href='" + ma[x][7] + "'>" + ma[x][1] + "</a></h4>ジャンル：" + ma[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + ma[x][2] + "&dirflg=w'>" + ma[x][2] + "</a><br/>営業時間：" + ma[x][8] + "<br/>定休日：" + ma[x][9] + "<br/>電話番号：<a hlef='tel:" + ma[x][14] + "'>" + ma[x][3] + "</a><br/>" + ma[x][6] + "<br/><br/>";
             }
+            else{
+                m.innerHTML += "<br/><h4>" + ma[x][1] + "</h4>ジャンル：" + ma[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + ma[x][2] + "&dirflg=w'>" + ma[x][2] + "</a><br/>営業時間：" + ma[x][8] + "<br/>定休日：" + ma[x][9] + "<br/>電話番号：<a hlef='tel:" + ma[x][14] + "'>" + ma[x][3] + "</a><br/>" + ma[x][6] + "<br/><br/>";
+            }
+           }
         }
     });
 });
@@ -76,9 +88,13 @@ $(function() {
             var f = document.getElementById("food");
             
            for(var x=20; x < 31; x++){
-              
-            f.innerHTML += "<br/><h4><a href='" + fo[x][7] + "'>" + fo[x][1] + "</a></h4>ジャンル：" + fo[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + fo[x][2] + "&dirflg=w'>" + fo[x][2] + "</a><br/>営業時間：" + fo[x][8] + "<br/>定休日：" + fo[x][9] + "<br/>電話番号：" + fo[x][3] + "<br/>" + fo[x][6] + "<br/><br/>";
+              if(fo[x][7]!=""){
+            f.innerHTML += "<br/><h4><a href='" + fo[x][7] + "'>" + fo[x][1] + "</a></h4>ジャンル：" + fo[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + fo[x][2] + "&dirflg=w'>" + fo[x][2] + "</a><br/>営業時間：" + fo[x][8] + "<br/>定休日：" + fo[x][9] + "<br/>電話番号：<a hlef='tel:" + fo[x][14] + "'>" + fo[x][3] + "</a><br/>" + fo[x][6] + "<br/><br/>";
             }
+            else{
+            f.innerHTML += "<br/><h4>" + fo[x][1] + "</h4>ジャンル：" + fo[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + fo[x][2] + "&dirflg=w'>" + fo[x][2] + "</a><br/>営業時間：" + fo[x][8] + "<br/>定休日：" + fo[x][9] + "<br/>電話番号：<a hlef='tel:" + fo[x][14] + "'>" + fo[x][3] + "</a><br/>" + fo[x][6] + "<br/><br/>";
+            }
+           }
         }
     });
 });
@@ -94,9 +110,13 @@ $(function() {
             var a = document.getElementById("a");
             
            for(var x=31; x <= 44; x++){
-              
-            a.innerHTML += "<br/><h4><a href='" + aa[x][7] + "'>" + aa[x][1] + "</a></h4>ジャンル：" + aa[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + aa[x][2] + "&dirflg=w'>" + aa[x][2] + "</a><br/>営業時間：" + aa[x][8] + "<br/>定休日：" + aa[x][9] + "<br/>電話番号：" + aa[x][3] + "<br/>" + aa[x][6] + "<br/><br/>";
+              if(aa[x][7]!=""){
+            a.innerHTML += "<br/><h4><a href='" + aa[x][7] + "'>" + aa[x][1] + "</a></h4>ジャンル：" + aa[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + aa[x][2] + "&dirflg=w'>" + aa[x][2] + "</a><br/>営業時間：" + aa[x][8] + "<br/>定休日：" + aa[x][9] + "<br/>電話番号：<a hlef='tel:" + aa[x][14] + "'>" + aa[x][3] + "</a><br/>" + aa[x][6] + "<br/><br/>";
             }
+            else{
+                a.innerHTML += "<br/><h4>" + aa[x][1] + "</h4>ジャンル：" + aa[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + aa[x][2] + "&dirflg=w'>" + aa[x][2] + "</a><br/>営業時間：" + aa[x][8] + "<br/>定休日：" + aa[x][9] + "<br/>電話番号：<a hlef='tel:" + aa[x][14] + "'>" + aa[x][3] + "</a><br/>" + aa[x][6] + "<br/><br/>";
+            }
+           }
         }
     });
 });
--- js/gojuon.js
+++ js/gojuon.js
@@ -13,8 +13,13 @@ $(function() {
             var a = document.getElementById("a");
             
            for(var x=1; x < 9; x++){
-              
-            a.innerHTML += "<br/><h4><a href='" + aa[x][7] + "'>" + aa[x][1] + "</a></h4>ジャンル：" + aa[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + aa[x][2] + "&dirflg=w'>"　+ aa[x][2] + "</a><br/>営業時間：" + aa[x][8] + "<br/>定休日：" + aa[x][9] + "<br/>電話番号：" + aa[x][3] + "<br/>" + aa[x][5] + "<br/><br/>";
+              if(aa[x][7]!=""){
+            a.innerHTML += "<br/><h4><a href='" + aa[x][7] + "'>" + aa[x][1] + "</a></h4>ジャンル：" + aa[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + aa[x][2] + "&dirflg=w'>"　+ aa[x][2] + "</a><br/>営業時間：" + aa[x][8] + "<br/>定休日：" + aa[x][9] + "<br/>電話番号：<a hlef='tel:" + aa[x][15] + "'>" + aa[x][3] + "</a><br/>" + aa[x][5] + "<br/><br/>";
+              }
+              else{
+             a.innerHTML += "<br/><h4>" + aa[x][1] + "</h4>ジャンル：" + aa[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + aa[x][2] + "&dirflg=w'>"　+ aa[x][2] + "</a><br/>営業時間：" + aa[x][8] + "<br/>定休日：" + aa[x][9] + "<br/>電話番号：<a hlef='tel:" + aa[x][15] + "'>" + aa[x][3] + "</a><br/>" + aa[x][5] + "<br/><br/>";
+            
+              }
             }
         }
     });
@@ -32,20 +37,16 @@ $(function() {
  
             // csvを配列に格納
             kk = $.csv()(data);
- 
-            // 挿入するHTMLを作成
-            //insert=csvList[10][3];
-            
-            // 上書きパターン
-             //$(target).html(insert);
             
             k = document.getElementById("ka");  
             
-           // k.innerHTML = "<a href='" + kk[12][7] + "'>" + kk[12][1] + "</a>";
-            
             for(var x=9; x < 17; x++){
-              
-            k.innerHTML += "<br/><h4><a href='" + kk[x][7] + "'>" + kk[x][1] + "</a></h4>ジャンル：" + kk[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr="+ kk[x][2] + "&dirflg=w'>"  + kk[x][2] + "</a><br/>営業時間：" + kk[x][8] + "<br/>定休日：" + kk[x][9] + "<br/>電話番号：" + kk[x][3] + "<br/>" + kk[x][5] + "<br/><br/>";
+              if(kk[x][7]!=""){
+            k.innerHTML += "<br/><h4><a href='" + kk[x][7] + "'>" + kk[x][1] + "</a></h4>ジャンル：" + kk[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr="+ kk[x][2] + "&dirflg=w'>"  + kk[x][2] + "</a><br/>営業時間：" + kk[x][8] + "<br/>定休日：" + kk[x][9] + "<br/>電話番号：<a hlef='tel:" + kk[x][15] + "'>" + kk[x][3] + "</a><br/>" + kk[x][5] + "<br/><br/>";
+            }
+            else{
+               k.innerHTML += "<br/><h4>" + kk[x][1] + "</h4>ジャンル：" + kk[x][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr="+ kk[x][2] + "&dirflg=w'>"  + kk[x][2] + "</a><br/>営業時間：" + kk[x][8] + "<br/>定休日：" + kk[x][9] + "<br/>電話番号：<a hlef='tel:" + kk[x][15] + "'>" + kk[x][3] + "</a><br/>" + kk[x][5] + "<br/><br/>"; 
+            }
             }
         }
     });
@@ -63,20 +64,17 @@ $(function() {
  
             // csvを配列に格納
             ss = $.csv()(data);
- 
-            // 挿入するHTMLを作成
-            //insert=csvList[10][3];
             
-            // 上書きパターン
-             //$(target).html(insert);
-            
-            s = document.getElementById("sa");  
-            
-           // k.innerHTML = "<a href='" + kk[12][7] + "'>" + kk[12][1] + "</a>";
+            s = document.getElementById("sa"); 
             
             for(var a=17; a < 27; a++){
-              
-            s.innerHTML += "<br/><h4><a href='" + ss[a][7] + "'>" + ss[a][1] + "</a></h4>ジャンル：" + ss[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + ss[a][2] + "&dirflg=w'>" + ss[a][2] + "</a><br/>営業時間：" + ss[a][8] + "<br/>定休日：" + ss[a][9] + "<br/>電話番号：" + ss[a][3] + "<br/>" + ss[a][5] + "<br/><br/>";
+              if(ss[a][7]!=""){
+            s.innerHTML += "<br/><h4><a href='" + ss[a][7] + "'>" + ss[a][1] + "</a></h4>ジャンル：" + ss[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + ss[a][2] + "&dirflg=w'>" + ss[a][2] + "</a><br/>営業時間：" + ss[a][8] + "<br/>定休日：" + ss[a][9] + "<br/>電話番号：<a hlef='tel:" + ss[a][15] + "'>" + ss[a][3] + "</a><br/>" + ss[a][5] + "<br/><br/>";
+            }
+            else{
+               s.innerHTML += "<br/><h4>" + ss[a][1] + "</h4>ジャンル：" + ss[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + ss[a][2] + "&dirflg=w'>" + ss[a][2] + "</a><br/>営業時間：" + ss[a][8] + "<br/>定休日：" + ss[a][9] + "<br/>電話番号：<a hlef='tel:" + ss[a][15] + "'>" + ss[a][3] + "</a><br/>" + ss[a][5] + "<br/><br/>";
+             
+            }
             }
         }
     });
@@ -94,20 +92,16 @@ $(function() {
  
             // csvを配列に格納
             tt = $.csv()(data);
- 
-            // 挿入するHTMLを作成
-            //insert=csvList[10][3];
-            
-            // 上書きパターン
-             //$(target).html(insert);
             
             t = document.getElementById("ta");  
             
-           // k.innerHTML = "<a href='" + kk[12][7] + "'>" + kk[12][1] + "</a>";
-            
             for(var a=27; a < 29; a++){
-              
-            t.innerHTML += "<br/><h4><a href='" + tt[a][7] + "'>" + tt[a][1] + "</a></h4>ジャンル：" + tt[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + tt[a][2] + "&dirflg=w'>"  + tt[a][2] + "</a><br/>営業時間：" + tt[a][8] + "<br/>定休日：" + tt[a][9] + "<br/>電話番号：" + tt[a][3] + "<br/>" + tt[a][5] + "<br/><br/>";
+              if(tt[a][7]!=""){
+            t.innerHTML += "<br/><h4><a href='" + tt[a][7] + "'>" + tt[a][1] + "</a></h4>ジャンル：" + tt[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + tt[a][2] + "&dirflg=w'>"  + tt[a][2] + "</a><br/>営業時間：" + tt[a][8] + "<br/>定休日：" + tt[a][9] + "<br/>電話番号：<a hlef='tel:" + tt[a][15] + "'>" + tt[a][3] + "</a><br/>" + tt[a][5] + "<br/><br/>";
+            }else{
+             t.innerHTML += "<br/><h4>" + tt[a][1] + "</h4>ジャンル：" + tt[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + tt[a][2] + "&dirflg=w'>"  + tt[a][2] + "</a><br/>営業時間：" + tt[a][8] + "<br/>定休日：" + tt[a][9] + "<br/>電話番号：<a hlef='tel:" + tt[a][15] + "'>" + tt[a][3] + "</a><br/>" + tt[a][5] + "<br/><br/>";
+            }
+            
             }
         }
     });
@@ -129,8 +123,12 @@ $(function() {
             n = document.getElementById("na");  
             
             for(var a=29; a < 32; a++){
-              
-            n.innerHTML += "<br/><h4><a href='" + nn[a][7] + "'>" + nn[a][1] + "</a></h4>ジャンル：" + nn[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + nn[a][2] + "&dirflg=w'>" + nn[a][2] + "</a><br/>営業時間：" + nn[a][8] + "<br/>定休日：" + nn[a][9] + "<br/>電話番号：" + nn[a][3] + "<br/>" + nn[a][5] + "<br/><br/>";
+              if(nn[a][7]!=""){
+            n.innerHTML += "<br/><h4><a href='" + nn[a][7] + "'>" + nn[a][1] + "</a></h4>ジャンル：" + nn[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + nn[a][2] + "&dirflg=w'>" + nn[a][2] + "</a><br/>営業時間：" + nn[a][8] + "<br/>定休日：" + nn[a][9] + "<br/>電話番号：<a hlef='tel:" + nn[a][15] + "'>" + nn[a][3] + "</a><br/>" + nn[a][5] + "<br/><br/>";
+            }
+            else{
+                n.innerHTML += "<br/><h4>" + nn[a][1] + "</h4>ジャンル：" + nn[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + nn[a][2] + "&dirflg=w'>" + nn[a][2] + "</a><br/>営業時間：" + nn[a][8] + "<br/>定休日：" + nn[a][9] + "<br/>電話番号：<a hlef='tel:" + nn[a][15] + "'>" + nn[a][3] + "</a><br/>" + nn[a][5] + "<br/><br/>";
+           }
             }
         }
     });
@@ -152,8 +150,12 @@ $(function() {
             h = document.getElementById("ha");  
             
             for(var a=32; a < 37; a++){
-              
-            h.innerHTML += "<br/><h4><a href='" + hh[a][7] + "'>" + hh[a][1] + "</a></h4>ジャンル：" + hh[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + hh[a][2] + "&dirflg=w'>"  + hh[a][2] + "</a><br/>営業時間：" + hh[a][8] + "<br/>定休日：" + hh[a][9] + "<br/>電話番号：" + hh[a][3] + "<br/>" + hh[a][5] + "<br/><br/>";
+              if(hh[a][7]!=""){
+            h.innerHTML += "<br/><h4><a href='" + hh[a][7] + "'>" + hh[a][1] + "</a></h4>ジャンル：" + hh[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + hh[a][2] + "&dirflg=w'>"  + hh[a][2] + "</a><br/>営業時間：" + hh[a][8] + "<br/>定休日：" + hh[a][9] + "<br/>電話番号：<a hlef='tel:" + hh[a][15] + "'>" + hh[a][3] + "</a><br/>" + hh[a][5] + "<br/><br/>";
+            }
+            else{
+             h.innerHTML += "<br/><h4>" + hh[a][1] + "</h4>ジャンル：" + hh[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + hh[a][2] + "&dirflg=w'>"  + hh[a][2] + "</a><br/>営業時間：" + hh[a][8] + "<br/>定休日：" + hh[a][9] + "<br/>電話番号：<a hlef='tel:" + hh[a][15] + "'>" + hh[a][3] + "</a><br/>" + hh[a][5] + "<br/><br/>";
+            }
             }
         }
     });
@@ -175,12 +177,17 @@ $(function() {
             m = document.getElementById("ma");  
             
             for(var a=37; a < 39; a++){
-              
-            m.innerHTML += "<br/><h4><a href='" + mm[a][7] + "'>" + mm[a][1] + "</a></h4>ジャンル：" + mm[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + mm[a][2] + "&dirflg=w'>"  + mm[a][2] + "</a><br/>営業時間：" + mm[a][8] + "<br/>定休日：" + mm[a][9] + "<br/>電話番号：" + mm[a][3] + "<br/>" + mm[a][5] + "<br/><br/>";
+              if(mm[a][7]!=""){
+            m.innerHTML += "<br/><h4><a href='" + mm[a][7] + "'>" + mm[a][1] + "</a></h4>ジャンル：" + mm[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + mm[a][2] + "&dirflg=w'>"  + mm[a][2] + "</a><br/>営業時間：" + mm[a][8] + "<br/>定休日：" + mm[a][9] + "<br/>電話番号：<a hlef='tel:" + mm[a][15] + "'>" + mm[a][3] + "</a><br/>" + mm[a][5] + "<br/><br/>";
+            }
+            else{
+                m.innerHTML += "<br/><h4>" + mm[a][1] + "</h4>ジャンル：" + mm[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + mm[a][2] + "&dirflg=w'>"  + mm[a][2] + "</a><br/>営業時間：" + mm[a][8] + "<br/>定休日：" + mm[a][9] + "<br/>電話番号：<a hlef='tel:" + mm[a][15] + "'>" + mm[a][3] + "</a><br/>" + mm[a][5] + "<br/><br/>";
+            }
             }
         }
     });
 });
+
 
 
 // $(function() {
@@ -205,7 +212,6 @@ $(function() {
 //     });
 // });
 
-
 $(function() {
     var target = '#ra';
     var hh;
@@ -221,8 +227,12 @@ $(function() {
             h = document.getElementById("ra");  
             
             for(var a=39; a < 43; a++){
-              
-            h.innerHTML += "<br/><h4><a href='" + hh[a][7] + "'>" + hh[a][1] + "</a></h4>ジャンル：" + hh[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + hh[a][2] + "&dirflg=w'>"  + hh[a][2] + "</a><br/>営業時間：" + hh[a][3] + "<br/>定休日：" + hh[a][4] + "<br/>電話番号：" + hh[a][6] + "<br/>" + hh[a][8] + "<br/><br/>";
+              if(hh[a][7]!=""){
+            h.innerHTML += "<br/><h4><a href='" + hh[a][7] + "'>" + hh[a][1] + "</a></h4>ジャンル：" + hh[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + hh[a][2] + "&dirflg=w'>"  + hh[a][2] + "</a><br/>営業時間：" + hh[a][8] + "<br/>定休日：" + hh[a][9] + "<br/>電話番号：<a hlef='tel:" + hh[a][15] + "'>" + hh[a][3] + "</a><br/>" + hh[a][5] + "<br/><br/>";
+            }
+            else{
+              h.innerHTML += "<br/><h4>" + hh[a][1] + "</h4>ジャンル：" + hh[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + hh[a][2] + "&dirflg=w'>"  + hh[a][2] + "</a><br/>営業時間：" + hh[a][8] + "<br/>定休日：" + hh[a][9] + "<br/>電話番号：<a hlef='tel:" + hh[a][15] + "'>" + hh[a][3] + "</a><br/>" + hh[a][5] + "<br/><br/>";
+            }
             }
         }
     });
@@ -243,8 +253,13 @@ $(function() {
             w = document.getElementById("wa");  
             
             for(var a=43; a <= 44; a++){
-              
-            w.innerHTML += "<br/><h4><a href='" + ww[a][7] + "'>" + ww[a][1] + "</a></h4>ジャンル：" + ww[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + ww[a][2] + "&dirflg=w'>"  + ww[a][2]  + "</a><br/>営業時間：" + ww[a][3] + "<br/>定休日：" + ww[a][4] + "<br/>電話番号：" + ww[a][6] + "<br/>" + ww[a][8] + "<br/><br/>";
+              if(ww[a][7]!=""){
+            w.innerHTML += "<br/><h4><a href='" + ww[a][7] + "'>" + ww[a][1] + "</a></h4>ジャンル：" + ww[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + ww[a][2] + "&dirflg=w'>"  + ww[a][2]  + "</a><br/>営業時間：" + ww[a][8] + "<br/>定休日：" + ww[a][9] + "<br/>電話番号：<a hlef='tel:" + ww[a][15] + "'>" + ww[a][3] + "</a><br/>" + ww[a][5] + "<br/><br/>";
+            }
+            else{
+                w.innerHTML += "<br/><h4>" + ww[a][1] + "</h4>ジャンル：" + ww[a][14] + "<br/>住所：<a href='http://maps.google.com/maps?saddr=&daddr=" + ww[a][2] + "&dirflg=w'>"  + ww[a][2]  + "</a><br/>営業時間：" + ww[a][8] + "<br/>定休日：" + ww[a][9] + "<br/>電話番号：<a hlef='tel:" + ww[a][15] + "'>" + ww[a][3] + "</a><br/>" + ww[a][5] + "<br/><br/>";
+            
+            }
             }
         }
     });
